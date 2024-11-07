94fb71dda9c154d17a34baf1236c13c036aaeea0
Caleb <calebkiune@gmail.com>
2024-11-07 20:57:41 +0300

    Adds crud lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data));
+  }, []);
+
+  function handleDeleteQuestion(id) {
+    setQuestions((prevQuestions) => prevQuestions.filter((question) => question.id !== id));
+  }
+
+  function handleUpdateQuestion(updatedQuestion) {
+    setQuestions((prevQuestions) =>
+      prevQuestions.map((question) =>
+        question.id === updatedQuestion.id ? updatedQuestion : question
+      )
+    );
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onAddQuestion={setQuestions} />
+      ) : (
+        <QuestionList
+          questions={questions}
+          onDeleteQuestion={handleDeleteQuestion}
+          onUpdateQuestion={handleUpdateQuestion}
+        />
+      )}
     </main>
   );
 }
